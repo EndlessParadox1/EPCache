61c6c0719fbc54b6b988d6526cff674bb37f1d34
EndlessParadox1 <endlessparadox250@gmail.com>
2024-05-03 17:52:12 +0800

    v1.8.1

--- etcd/startup.sh
+++ etcd/startup.sh
@@ -5,19 +5,19 @@ cluster='infra1=http://localhost:2380,infra2=http://localhost:12380,infra3=http:
 
 # etcd1
 nohup etcd --name infra1 --listen-client-urls http://localhost:2379 --advertise-client-urls http://localhost:2379 \
-  --listen-peer-urls http://localhost:2380 --initial-advertise-peer-urls http://localhost:2380 \
+  --initial-advertise-peer-urls http://localhost:2380 --listen-peer-urls http://localhost:2380 \
   --initial-cluster-token $token --initial-cluster $cluster --initial-cluster-state new \
   --log-level warn >etcd1.log 2>&1 &
 
 # etcd2
 nohup etcd --name infra2 --listen-client-urls http://localhost:12379 --advertise-client-urls http://localhost:12379 \
-  --listen-peer-urls http://localhost:12380 --initial-advertise-peer-urls http://localhost:12380 \
+  --initial-advertise-peer-urls http://localhost:12380 --listen-peer-urls http://localhost:12380 \
   --initial-cluster-token $token --initial-cluster $cluster --initial-cluster-state new\
   --log-level warn >etcd2.log 2>&1 &
 
 # etcd3
 nohup etcd --name infra3 --listen-client-urls http://localhost:22379 --advertise-client-urls http://localhost:22379 \
-  --listen-peer-urls http://localhost:22380 --initial-advertise-peer-urls http://localhost:22380 \
+   --initial-advertise-peer-urls http://localhost:22380 --listen-peer-urls http://localhost:22380\
   --initial-cluster-token $token --initial-cluster $cluster --initial-cluster-state new\
   --log-level warn >etcd3.log 2>&1 &
 
