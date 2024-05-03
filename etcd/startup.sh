2800fa8f78d65048f47b1e5bac4710856a4d7869
EndlessParadox1 <endlessparadox250@gmail.com>
2024-05-03 13:30:17 +0800

    v1.8.1

--- etcd/startup.sh
+++ etcd/startup.sh
@@ -7,19 +7,19 @@ cluster='infra1=http://localhost:2380,infra2=http://localhost:12380,infra3=http:
 nohup etcd --name infra1 --listen-client-urls http://localhost:2379 --advertise-client-urls http://localhost:2379 \
   --listen-peer-urls http://localhost:2380 --initial-advertise-peer-urls http://localhost:2380 \
   --initial-cluster-token $token --initial-cluster $cluster --initial-cluster-state new \
-  --log-level warn --log-output etcd1.log &
+  --log-level warn >etcd1.log 2>&1 &
 
 # etcd2
 nohup etcd --name infra2 --listen-client-urls http://localhost:12379 --advertise-client-urls http://localhost:12379 \
   --listen-peer-urls http://localhost:12380 --initial-advertise-peer-urls http://localhost:12380 \
-  --initial-cluster-token $token --initial-cluster $cluster --initial-cluster-state \
-  --log-level warn --log-output etcd2.log &
+  --initial-cluster-token $token --initial-cluster $cluster --initial-cluster-state new\
+  --log-level warn >etcd2.log 2>&1 &
 
 # etcd3
 nohup etcd --name infra3 --listen-client-urls http://localhost:22379 --advertise-client-urls http://localhost:22379 \
   --listen-peer-urls http://localhost:22380 --initial-advertise-peer-urls http://localhost:22380 \
-  --initial-cluster-token $token --initial-cluster $cluster --initial-cluster-state \
-  --log-level warn --log-output etcd3.log &
+  --initial-cluster-token $token --initial-cluster $cluster --initial-cluster-state new\
+  --log-level warn >etcd3.log 2>&1 &
 
 # run `./startup.sh` to start the etcd cluster in bg.
 # This is just an example of three nodes, you may edit the file as you like to build your own etcd cluster.
